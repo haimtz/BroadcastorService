f9a42bda31644fee9de9bf9de4e396790dbd86fe
haimtz <haimtzabar@gmail.com>
2016-02-10 16:57:34 +0200

    WCF

---
+++ BroadcastorService/IBroadcastorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BroadcastorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IBroadcastorService
+    {
+        [OperationContract(IsOneWay = true)]
+        void RegisterClient(string clientName);
+    }
+
+    [DataContract]
+    public class EventDataType
+    {
+        [DataMember]
+        public string ClientName { get; set; }
+
+        [DataMember]
+        public string EventMessage { get; set; }
+    }
+}
